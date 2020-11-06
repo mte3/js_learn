5871e8cfff7f691e139450cf1de8b90da634466f
莫金华 <mte2425379518@163.com>
2020-11-06 21:17:50 +0800

    001 ..

--- 猜字/js/network.js
+++ 猜字/js/network.js
@@ -4,6 +4,7 @@ function request(config) {
         baseURL:'http://8.129.111.60/knowledge/servlet',
         // baseURL:'http://123.207.32.32:8000/api/h8/',
         timeout: 5000,
+        changeOrigin:true, //允许跨域
     })
     return instance(config)
 }
