ec984d2a1a07ba7b536ecfadffbc9ad517e85adb
mte1 <mte2425379518@163.com>
2020-11-06 11:14:53 +0800

    09.

--- 猜字/js/network.js
+++ 猜字/js/network.js
@@ -1,6 +1,8 @@
 //网络请求
 function request(config) {
     const instance = axios.create({
+        baseURL:'http://8.129.111.60/knowledge/servlet',
+        // baseURL:'http://123.207.32.32:8000/api/h8/',
         timeout: 5000,
     })
     return instance(config)
